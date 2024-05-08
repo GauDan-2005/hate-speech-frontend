6133052953b5e438f6dff8f7ac4ac269541b89fa
GauDan-2005 <gauravdan2005@gmail.com>
2024-05-09 02:58:14 +0530

    link updaed

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import axios from "axios";
 import "./index.css";
 import App from "./App";
 
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://hate-speech-backend.onrender.com";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
